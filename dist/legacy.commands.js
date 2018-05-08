2cdcf287fbe169b8a744bbef007323f6495a5b24
Nazim Lachter <lachtater@gmail.com>
2018-05-08 02:25:12 +0200

    Mise à jour de dist

--- dist/legacy.commands.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-
-    safeMode: spawnName => Game.spawns[spawnName].room.controller.activateSafeMode()
-
-};
-
-// Game.spawns['Spawn1'].room.createConstructionSite( 23, 22, STRUCTURE_TOWER );
